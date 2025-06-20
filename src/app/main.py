637c360e5d66940c94ac0ea4e34d7e3cab2c8db4
Daniel A. <daniel@pointe.dev>
2025-06-20 18:27:12 +0200

    empty /

--- src/app/main.py
+++ src/app/main.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from gradio.routes import mount_gradio_app
 from app.recommender import recommend
 import gradio as gr
 
 app = FastAPI()
 
-gradio_ui = gr.Interface(
-    fn=recommend, inputs="text", outputs="text", title="Crypto Assistant"
-)
-app = mount_gradio_app(app, gradio_ui, path="/")
+# gradio_ui = gr.Interface(
+#     fn=recommend, inputs="text", outputs="text", title="Crypto Assistant"
+# )
+# app = mount_gradio_app(app, gradio_ui, path="/ui")
+
+
+@app.get("/")
+def index():
+    """
+    Index method.
+    """
+    return {"text": "Welcome"}
 
 
 @app.get("/ping")
