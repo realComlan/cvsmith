49c036cb2f11f8356345d5ea6877cb3ea762c77a
Daniel A. <daniel@pointe.dev>
2025-06-20 18:47:59 +0200

    flat out simple routes for railway debug

--- src/app/main.py
+++ src/app/main.py
@@ -4,18 +4,18 @@ from app.recommender import recommend
 
 app = FastAPI()
 
-gradio_ui = gr.Interface(
-    fn=recommend, inputs="text", outputs="text", title="Crypto Assistant"
-)
-app = gr.mount_gradio_app(app, gradio_ui, path="/")
+# gradio_ui = gr.Interface(
+#     fn=recommend, inputs="text", outputs="text", title="Crypto Assistant"
+# )
+# app = gr.mount_gradio_app(app, gradio_ui, path="/")
 
 
-# @app.get("/")
-# def index():
-#     """
-#     Index method.
-#     """
-#     return {"text": "Welcome"}
+@app.get("/")
+def index():
+    """
+    Index method.
+    """
+    return {"text": "Welcome"}
 
 
 @app.get("/ping")
