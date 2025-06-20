40f511c63bd424c1427187de59923b8047a8e253
Daniel A. <daniel@pointe.dev>
2025-06-20 20:07:15 +0200

    fix main.py

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,6 @@
 import gradio as gr
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from app.recommender import recommend
 
@@ -8,7 +9,15 @@ app = FastAPI()
 gradio_ui = gr.Interface(
     fn=recommend, inputs="text", outputs="text", title="Crypto Assistant"
 )
-app = gr.mount_gradio_app(app, gradio_ui, path="/")
+app = gr.mount_gradio_app(app, gradio_ui, path="/ui")
+
+
+@app.get("/")
+def home():
+    """
+    Index page
+    """
+    return RedirectResponse(status_code=302, url="/ui")
 
 
 @app.get("/ping")
