00b7f9ca6cff24f2572e46ad8685b25f00876131
Daniel A. <daniel@pointe.dev>
2025-06-20 18:33:58 +0200

    updated route /

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,13 @@
+import gradio as gr
 from fastapi import FastAPI
+from app.recommender import recommend
 
 app = FastAPI()
 
-# gradio_ui = gr.Interface(
-#     fn=recommend, inputs="text", outputs="text", title="Crypto Assistant"
-# )
-# app = mount_gradio_app(app, gradio_ui, path="/ui")
+gradio_ui = gr.Interface(
+    fn=recommend, inputs="text", outputs="text", title="Crypto Assistant"
+)
+app = gr.mount_gradio_app(app, gradio_ui, path="/")
 
 
 @app.get("/")
