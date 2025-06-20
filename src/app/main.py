ab592e5706b80147f0d4271e45198cb4198b9644
Daniel A. <daniel@pointe.dev>
2025-06-20 18:02:32 +0200

    removed redirection for railway's sake

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
 from gradio.routes import mount_gradio_app
 from app.recommender import recommend
 import gradio as gr
@@ -9,15 +8,7 @@ app = FastAPI()
 gradio_ui = gr.Interface(
     fn=recommend, inputs="text", outputs="text", title="Crypto Assistant"
 )
-app = mount_gradio_app(app, gradio_ui, path="/ui")
-
-
-@app.get("/")
-def index():
-    """
-    Index method.
-    """
-    return RedirectResponse(url="/ui", status_code=302)
+app = mount_gradio_app(app, gradio_ui, path="/")
 
 
 @app.get("/ping")
